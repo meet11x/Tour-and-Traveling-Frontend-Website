f751b7ab84212a9950dad5e0ee7aa6f5c0dfa0c8
meet11x <meet11705@gmail.com>
2024-04-07 21:02:27 +0530

    addvdfbfbfb
    Co-Authored-By: meet11x <159559705+meet11x@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Sticky Navbar
+// $(window).scroll(function () {
+//   if ($(this).scrollTop() > 45) {
+//     $(".navbar").addClass("sticky-top shadow-sm");
+//   } else {
+//     $(".navbar").removeClass("sticky-top shadow-sm");
+//   }
+// });
+
+// Back to top button
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+    $(".back-to-top").fadeIn("slow");
+  } else {
+    $(".back-to-top").fadeOut("slow");
+  }
+});
+$(".back-to-top").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+  return false;
+});
